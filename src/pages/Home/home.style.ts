a2bc8023a40fa0c928a34da578d0ccaea8acae0d
Mickel-Barros <mickelbarros@yahoo.com>
2022-11-03 14:34:21 -0300

    Refactor: styled components

--- src/pages/Home/home.style.ts
+++ src/pages/Home/home.style.ts
@@ -1,84 +1,92 @@
 import styled from "styled-components";
-import colors from "../../styles/theme/colors";
-import typography from "../../styles/theme/typography";
-const Main = styled.main`
-    background: ${colors.brand.tertiary};
+import { Input } from "../../components/Input/input.style";
+import {Text} from "../../components/Text";
+import { Button } from "../../components/Button/button.style";
+export const Main = styled.main`
+    background: ${({theme}) => theme.colors.brand.tertiary};
     display: flex;
     justify-content: center;
     height: 100vh;
     align-items: center;
     flex-direction: column;
-    .map{
-        position: fixed;
-        z-index: 0;
-    }
-    .logo{
-        width: 250px;
-        z-index: 1;
-    }
-    h2{
-        text-align: center;
-        z-index: 1;
-        margin-bottom: 30px;
-        margin-top: 30px;
-        width: 250px;
-    }
-    .diviser{
-        width: 280px;
-        height: 1px;
-        background: ${colors.grey.grey3};
-        z-index: 1;
-        margin-bottom: 50px;
-    }
-    .diviserTwo{
-        width: 280px;
-        height: 1px;
-        background: ${colors.grey.grey3};
-        z-index: 1;
-        margin-bottom: 30px;
-    }
-    input{
-        height: 50px;
-        width: 280px;
-        z-index: 1;
-        margin-bottom: 25px;
-        ::placeholder {
-            font-size: ${typography.sizes.size2};
-        }
-    }
-    form{
-        width: 280px;
-        height: 250px;
-        z-index: 1;
-        margin-bottom: 30px;
-    }
-    button{
-        height: 50px;
-        width: 280px;
-        z-index: 1;
-    }
-    .openModal{
-        background: none;
-        color: ${colors.brand.primary};
-        font-size: ${typography.sizes.size3};
-        font-weight: 600;
-        width: 100px;
-        height: 30px;
-        margin-top: 12px;
-    }
-    .forgotPassword{
-        width: 280px;
-        height: 50px;
-        display: flex;
-        justify-content: center;
-    }
-    p{
-        z-index: 1;
-        margin-top: 20px;
-    }
-    .submitBtn{
-        border: 1px solid ${colors.brand.primary};
-    }
-    
 `
-export default Main;
+export const BackgroundImg = styled.img`
+    position: fixed;
+    z-index: 0;
+`
+export const Logo = styled.img`
+    width: 250px;
+    z-index: 1;
+`
+export const Diviser = styled.div`
+    width: 280px;
+    height: 1px;
+    background: ${({theme}) => theme.colors.grey.grey3};
+    z-index: 1;
+    margin-bottom: 50px;
+`
+export const DiviserTwo = styled.div`
+    width: 280px;
+    height: 1px;
+    background: ${({theme}) => theme.colors.grey.grey3};
+    z-index: 1;
+    margin-bottom: 30px;
+`
+export const ForgotPassword = styled.div`
+    width: 280px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+`
+export const Form = styled.form`
+    width: 280px;
+    height: 250px;
+    z-index: 1;
+    margin-bottom: 30px;
+`
+export const InputEmail = styled(Input)`
+    height: 50px;
+    width: 280px;
+    z-index: 1;
+    margin-bottom: 25px;
+    ::placeholder {
+        font-size: ${({theme}) => theme.typography.sizes.size2};
+    }
+`
+export const InputPassword = styled(Input)`
+    height: 50px;
+    width: 280px;
+    z-index: 1;
+    margin-bottom: 25px;
+    ::placeholder {
+        font-size: ${({theme}) => theme.typography.sizes.size2};
+    }
+`
+export const H2 = styled(Text)`
+    text-align: center;
+    z-index: 1;
+    margin-bottom: 30px;
+    margin-top: 30px;
+    width: 250px;
+`
+export const Forgot = styled(Text)`
+    z-index: 1;
+    margin-top: 20px;
+`
+export const SubmitBtn = styled(Button)`
+    border: 1px solid ${({theme}) => theme.colors.brand.primary};
+    height: 50px;
+    width: 280px;
+    z-index: 1;
+`
+export const OpenModal = styled(Button)`
+    background: none;
+    color: ${({theme}) => theme.colors.brand.primary};
+    font-size: ${({theme}) => theme.typography.sizes.size3};
+    font-weight: 600;
+    width: 100px;
+    margin-top: 3px;
+    height: 50px;
+    z-index: 1;
+`
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,25 @@
 import React from "react";
 import Map from "../../assets/img/Map.svg";
-import Main from "./home.style";
-import YourTrip from "../../assets/img/YourTrip.svg"
-import {Text} from "../../components/Text";
-import { Input } from "../../components/Input/input.style";
-import { Button } from "../../components/Button/button.style";
+import YourTrip from "../../assets/img/YourTrip.svg";
+import { Main, BackgroundImg, Logo, SubmitBtn, Diviser, DiviserTwo, Form, ForgotPassword, InputEmail, InputPassword, H2, OpenModal, Forgot} from "./home.style";
 const LandingPage: React.FC = () => {
     return(
         <Main>
-            <img className="map" src={Map} alt="mapa do mundo" />
-            <img className="logo" src={YourTrip} alt="YourTrip" />
-            <Text color="grey4" fontWeight={400} tag="h2" size="size1">A mais nova rede social de viagens</Text>
-            <div className="diviser"/>
-            <form>
-                <Input placeholder="Digite seu e-mail" type="email"/>
-                <Input placeholder="Digite sua senha" type="password"/>
-                <Button buttonType="primary" type="submit">Entrar</Button>
-                <div className="forgotPassword">
-                    <Text color="grey4" fontWeight={600} tag="p" size="size3">Esqueçeu sua senha?</Text>
-                    <Button className="openModal" buttonType="primary" type="submit">Clique aqui</Button>
-                </div>
-            </form>
-            <div className="diviserTwo"/>
-            <Button className="submitBtn" buttonType="secondary" type="submit">Quero me cadastrar</Button>
+            <BackgroundImg src={Map} alt="mapa do mundo"/>
+            <Logo src={YourTrip} alt="YourTrip" />
+            <H2 color="grey4" fontWeight={400} tag="h2" size="size1">A mais nova rede social de viagens</H2>
+            <Diviser/>
+            <Form>
+                <InputEmail placeholder="Digite seu e-mail" type="email"/>
+                <InputPassword placeholder="Digite sua senha" type="password"/>
+                <SubmitBtn buttonType="primary" type="submit">Entrar</SubmitBtn>
+                <ForgotPassword>
+                    <Forgot color="grey4" fontWeight={600} tag="p" size="size3">Esqueçeu sua senha?</Forgot>
+                    <OpenModal buttonType="primary" type="submit">Clique aqui</OpenModal>
+                </ForgotPassword>
+            </Form>
+            <DiviserTwo/>
+            <SubmitBtn buttonType="secondary" type="submit">Quero me cadastrar</SubmitBtn>
         </Main>
     )
 }
