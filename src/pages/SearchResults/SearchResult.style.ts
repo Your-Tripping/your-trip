7d707fb4614be6ed3846e6b34a9ad5987c02c4e5
Fernando Croccoli <nandique@gmail.com>
2022-11-08 23:14:02 -0300

    feat: return button and display target added

--- src/pages/SearchResults/SearchResult.style.ts
+++ src/pages/SearchResults/SearchResult.style.ts
@@ -6,4 +6,29 @@ export const SearchMain = styled.div`
   justify-content: center;
   position: relative;
   top: 80px;
+
+  & > div {
+    display: flex;
+    flex-direction: row;
+    margin: 16px 108px 0;
+    background-color: ${({ theme }) => theme.colors.grey.grey1};
+    border-radius: 8px;
+    padding: 16px;
+
+    & svg {
+      fill: ${({ theme }) => theme.colors.brand.primary};
+      display: flex;
+      align-items: center;
+    }
+
+    & p {
+      font-size: ${({ theme }) => theme.typography.sizes.size2};
+      align-self: center;
+      margin: auto;
+    }
+
+    & span {
+      color: ${({ theme }) => theme.colors.brand.primary};
+    }
+  }
 `;
--- src/pages/SearchResults/index.tsx
+++ src/pages/SearchResults/index.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
 import SearchDisplay from "../../components/SearchDisplay";
-import * as S from "./SearchResult.style"
+import { SearchContext } from "../../contexts/SearchContext";
+import * as S from "./SearchResult.style";
 
 const SearchPage = () => {
+  const { filter, target } = useContext(SearchContext);
+
   return (
     <S.SearchMain>
       <Header></Header>
+      <div>
+        <Link to={"/dashboard"}>
+          <AiOutlineArrowLeft />
+        </Link>
+        {filter !== "none" && <p>Mostrando resultados para: <span>{target}</span></p>}
+      </div>
       <SearchDisplay></SearchDisplay>
     </S.SearchMain>
   );
